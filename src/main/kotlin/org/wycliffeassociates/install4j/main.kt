ff79f04b6b9e0a3390e0b855534c203f23209046
Joe <jsarabia1247@gmail.com>
2021-08-23 11:06:59 -0400

    updates

---
+++ src/main/kotlin/org/wycliffeassociates/install4j/main.kt
@@ -0,0 +1,37 @@
+package org.wycliffeassociates.install4j
+
+import com.install4j.api.launcher.Variables
+import com.install4j.api.update.ApplicationDisplayMode
+import com.install4j.api.update.UpdateChecker
+import javafx.geometry.Pos
+import tornadofx.*
+
+const val version = 1
+
+fun main() {
+    launch<DemoApp>()
+}
+
+class DemoApp : App(MainView::class)
+
+class MainView : View() {
+    override val root = vbox {
+        alignment = Pos.CENTER
+        prefWidth = 100.0
+        prefHeight = 100.0
+        label("Version $version")
+        button("Update") {
+            setOnAction {
+                val updateUrl: String = Variables.getCompilerVariable("sys.updatesUrl")
+                val updateDescriptor = UpdateChecker.getUpdateDescriptor(updateUrl, ApplicationDisplayMode.GUI)
+                if (updateDescriptor.possibleUpdateEntry != null) {
+                    dialog {
+                        text = "meep"
+                    }
+                }
+            }
+        }
+    }
+
+
+}
